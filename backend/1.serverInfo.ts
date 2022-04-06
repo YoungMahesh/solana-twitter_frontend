27e6fa30c82339067f6354d4fd306adbd906ecb8
Mahesh Ghamand <mahesh471978@gmail.com>
2022-04-06 14:49:09 +0530

    removed messages and counter

--- backend/1.serverInfo.ts
+++ backend/1.serverInfo.ts
@@ -1,31 +1,24 @@
 interface ServerInfoInt {
-   readonly [val: string]: {
-        networkUrl: string
-        contracts: {
-            messages: string
-            counter: string
-            twitter: string
-        }
+  readonly [net: string]: {
+    networkUrl: string
+    contracts: {
+      twitter: string
     }
+  }
 }
-
 
 export const serverInfo: ServerInfoInt = {
-    localnet: {
-        networkUrl: 'http://127.0.0.1:8899',
-        contracts: {
-            messages: 'BMkLrsFDEUtW46wgiXhES7pTiRGpfSF5b7FsCbYCq4qo',
-            counter: '4HozAEDcAaTGxV9KZx34gxHAEAPsFJNaoqqgupHTUnT7',
-            twitter: 'HC4NZEsuGoVvbxBypDFjuFLRakQussGc5mfh9cGNHDgY',
-        },
+  localnet: {
+    networkUrl: "http://127.0.0.1:8899",
+    contracts: {
+      twitter: "HC4NZEsuGoVvbxBypDFjuFLRakQussGc5mfh9cGNHDgY",
     },
-    devnet: {
-        networkUrl: 'https://api.devnet.solana.com',
-        contracts: {
-            messages: 'BMkLrsFDEUtW46wgiXhES7pTiRGpfSF5b7FsCbYCq4qo',
-            counter: '4HozAEDcAaTGxV9KZx34gxHAEAPsFJNaoqqgupHTUnT7',
-            twitter: 'HC4NZEsuGoVvbxBypDFjuFLRakQussGc5mfh9cGNHDgY'
-        }
+  },
+  devnet: {
+    networkUrl: "https://api.devnet.solana.com",
+    contracts: {
+      twitter: "HC4NZEsuGoVvbxBypDFjuFLRakQussGc5mfh9cGNHDgY",
     },
+  },
 }
-export const defaultNetwork = 'devnet'
+export const defaultNetwork = "devnet"
--- backend/2.web3Provider.ts
+++ backend/2.web3Provider.ts
@@ -1,38 +1,24 @@
-import { Idl, Program, Provider, Wallet } from "@project-serum/anchor";
-import { Connection } from "@solana/web3.js";
-import { defaultNetwork, serverInfo } from "./1.serverInfo";
-import MessagesIdl from './messages.json'
-import CounterIdl from './counter.json'
-import TwitterIdl from './twitter.json'
+import { Idl, Program, Provider, Wallet } from "@project-serum/anchor"
+import { Connection } from "@solana/web3.js"
+import { defaultNetwork, serverInfo } from "./1.serverInfo"
+import TwitterIdl from "./twitter.json"
 
 export const getServerInfo = (network: string) => {
-    if(serverInfo[network]) return serverInfo[network]
-    return serverInfo[defaultNetwork]
+  if (serverInfo[network]) return serverInfo[network]
+  return serverInfo[defaultNetwork]
 }
 
-const getProvider = (network: string, wallet: Wallet,) => {
-    const networkUrl = getServerInfo(network).networkUrl
-    const connection = new Connection(networkUrl)
-    return new Provider(connection, wallet, {})
-}
-
-
-export const getMessagesProgram = (network: string, wallet: Wallet) => {
-    const provider = getProvider(network, wallet)
-    const msgBoxAddr = getServerInfo(network).contracts.messages
-
-    return new Program(MessagesIdl as Idl, msgBoxAddr, provider)
-}
-
-export const getCounterProgram = (network: string, wallet: Wallet) => {
-    const provider = getProvider(network, wallet)
-    const counterAddr = getServerInfo(network).contracts.counter
-    return new Program(CounterIdl as Idl, counterAddr, provider)
+const getProvider = (network: string, wallet: Wallet) => {
+  const networkUrl = getServerInfo(network).networkUrl
+  const connection = new Connection(networkUrl)
+  return new Provider(connection, wallet, {})
 }
 
 export const getTwitterProgram = (network: string, wallet: Wallet) => {
-    const twitterAddr = getServerInfo(network).contracts.twitter
-    return new Program(TwitterIdl as Idl, twitterAddr, getProvider(network, wallet))
+  const twitterAddr = getServerInfo(network).contracts.twitter
+  return new Program(
+    TwitterIdl as Idl,
+    twitterAddr,
+    getProvider(network, wallet)
+  )
 }
-
-
